04deaf62298899d84a6cda8e4a6450db3358464d
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-08-01 14:41:57 -0400

    Refactored HTML Layout
    - refactoring the entire HTML layout for a more 7DTD theme.

--- script.js
+++
@@ -1,9 +0,0 @@
-const attributeButtons = document.querySelectorAll(".attribute-heading");
-
-attributeButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        const attributeSection = button.parentElement;
-        const perkList = attributeSection.querySelector(".perk-list");
-        perkList.classList.toggle("open");
-    });
-});
